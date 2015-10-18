0715876ce53bdadecbb6e206b5f809c531c183d4
Amin sheikhi <aminsheikhi@gmail.com>
2015-10-18 23:18:10 +0330

    Added DataAccess project in Application directory for business

---
+++ Application/DataAccess/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+using Core.DataAccess;
+using Domain;
+
+namespace DataAccess
+{
+    public class AppDbContext : CoreDbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
