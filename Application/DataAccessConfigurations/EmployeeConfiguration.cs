3041633e4da9db4ef36e7c16e49210d62ddab803
Amin sheikhi <aminsheikhi@gmail.com>
2015-11-14 00:22:14 +0330

    Added Entity framework Configuration Project on Application Forlder

---
+++ Application/DataAccessConfigurations/EmployeeConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity.ModelConfiguration;
+using Domain;
+
+namespace DataAccessConfigurations
+{
+    public class EmployeeConfiguration : EntityTypeConfiguration<Employee>
+    {
+        public EmployeeConfiguration()
+        {
+            Property(e => e.RowVersion).IsRowVersion().IsConcurrencyToken();
+        }
+    }
+}
