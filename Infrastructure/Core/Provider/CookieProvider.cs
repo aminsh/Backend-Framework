b419b1ebc5faa07fcd36c38e54e6d0bd4fec65cc
Amin sheikhi <aminsheikhi@gmail.com>
2015-11-15 00:19:43 +0330

    Added Cookie Provider

---
+++ Infrastructure/Core/Provider/CookieProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Web;
+
+namespace Core.Provider
+{
+    public interface ICookieProvider
+    {
+        void Set(string key, object value);
+
+        string Get(string key);
+    }
+
+    public class CookieProvider : ICookieProvider
+    {
+        public void Set(string key, object value)
+        {
+            HttpContext.Current.GetOwinContext().Response.Cookies.Append(key, value.ToString());
+        }
+
+        public string Get(string key)
+        {
+            return HttpContext.Current.GetOwinContext().Request.Cookies[key];
+        }
+    }
+}
