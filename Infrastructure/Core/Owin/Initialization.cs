8519f35b266145187e4b12be6c31326b94ba0809
Amin sheikhi <aminsheikhi@gmail.com>
2015-11-15 00:22:54 +0330

    Added Owin initialization

---
+++ Infrastructure/Core/Owin/Initialization.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Http;
+using System.Web.Http.Dependencies;
+using Core.IOC;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Practices.Unity;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using Utility;
+
+namespace Core.Owin
+{
+    public static class  OwinConfiguration
+    {
+        public static void Register(IAppBuilder app, IDependencyResolver resolver)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Account"),
+                CookieSecure = CookieSecureOption.SameAsRequest
+            });
+
+            var config = new HttpConfiguration();
+
+            config.MapHttpAttributeRoutes();
+
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+               new CamelCasePropertyNamesContractResolver();
+            config.Formatters.JsonFormatter.UseDataContractJsonSerializer = false;
+
+            config.Filters.Add(new AuthorizeAttribute());
+            config.DependencyResolver = resolver;
+            
+            
+            app.UseWebApi(config);
+            app.MapSignalR();
+        }
+    }
+}
